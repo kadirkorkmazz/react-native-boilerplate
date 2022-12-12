b91e322ada532d20f328d184e9ed4e4c9aa75668
Kadir Korkmaz <kadir.korkmaz@wunder.is>
2022-12-12 21:59:30 +0300

    add husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
 module.exports = {
   root: true,
-  extends: "@react-native-community",
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "react-hooks/exhaustive-deps": "off",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "prettier/prettier": [
-          "error",
+        'react-hooks/exhaustive-deps': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'prettier/prettier': [
+          'error',
           {},
           {
             bracketSpacing: true,
             singleQuote: true,
-            trailingComma: "all",
+            trailingComma: 'all',
           },
         ],
       },
     },
   ],
 };
-
