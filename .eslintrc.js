2ebd6921fe3155e7999879d5813e94a5fb8d8ae1
Kadir Korkmaz <kadir.korkmaz@wunder.is>
2022-12-12 21:49:51 +0300

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: "@react-native-community",
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
+        "react-hooks/exhaustive-deps": "off",
+        "@typescript-eslint/no-unused-vars": "warn",
+        "prettier/prettier": [
+          "error",
+          {},
+          {
+            bracketSpacing: true,
+            singleQuote: true,
+            trailingComma: "all",
+          },
+        ],
       },
     },
   ],
 };
+
